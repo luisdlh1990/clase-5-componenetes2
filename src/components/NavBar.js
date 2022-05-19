8896d5ecca02a2b791743765fdc48b3fd6bd7467
luis de la hoz <luisdlh1990@gmail.com>
2022-05-18 23:16:50 -0300

    changes

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar =()=>{
+    return(
+        <>
+        <header>
+        <strong><em>React-Tube-App</em></strong>
+        </header>
+        </>
+    );
+}
+
+export default NavBar;
